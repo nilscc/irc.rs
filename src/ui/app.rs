55d1ade7ecfe49a8a576ff81e94af28c1fd1aac5
Nils <mail@nils.cc>
2024-12-26 23:04:44 +0100

    Store settings in local storage

--- src/ui/app.rs
+++ src/ui/app.rs
@@ -1,8 +1,13 @@
+use gloo::{
+    console,
+    storage::{LocalStorage, Storage},
+};
+use serde::{Deserialize, Serialize};
 use web_sys::HtmlInputElement;
 use yew::prelude::*;
 use yew_router::prelude::*;
 
-#[derive(Debug, Clone, PartialEq)]
+#[derive(Debug, Clone, PartialEq, Serialize, Deserialize)]
 struct Settings {
     host: String,
 }
@@ -51,10 +56,12 @@ fn SettingsPage(props: &SettingsProps) -> Html {
     let onsubmit = Callback::from({
         let host_ref = host_ref.clone();
         let set_settings = props.set_settings.clone();
+        let nav = use_navigator().unwrap();
         move |e: SubmitEvent| {
             e.prevent_default();
             let host = host_ref.cast::<HtmlInputElement>().unwrap().value();
-            set_settings.emit(Settings { host })
+            set_settings.emit(Settings { host });
+            nav.push(&Route::Home);
         }
     });
 
@@ -73,24 +80,54 @@ fn SettingsPage(props: &SettingsProps) -> Html {
     }
 }
 
-fn switch_route(route: Route, settings: &Option<Settings>) -> Html {
-    match route {
-        Route::Home => html! {
-            <HomePage settings={settings.clone()}/>
-        },
-        Route::Settings => html! {
-            <SettingsPage />
-        },
-    }
-}
-
 #[function_component]
 pub fn App() -> Html {
-    let settings = use_state(|| None::<Settings>);
+    // load settings from local storage (if available)
+    let settings = use_state_eq(|| match LocalStorage::get("settings") {
+        Ok(settings) => Some(settings),
+        Err(err) => {
+            console::error!(format!(
+                "Error retrieving settings from local storage:\n{err}"
+            ));
+            None
+        }
+    });
+
+    // store new settings in local storage and update UI state
+    let set_settings = Callback::from({
+        let settings = settings.clone();
+        move |new_settings: Settings| {
+            console::log!(format!("new settings: {new_settings:?}"));
+
+            // set local storage
+            if let Err(err) = LocalStorage::set("settings", &new_settings) {
+                console::error!(format!("Error storing settings:\n{err}"));
+            }
+
+            // update state
+            settings.set(Some(new_settings));
+        }
+    });
+
+    // main application routing
+    let switch = Callback::from({
+        move |route: Route| {
+            let settings = (*settings).clone();
+            let set_settings = set_settings.clone();
+            match route {
+                Route::Home => html! {
+                    <HomePage {settings} />
+                },
+                Route::Settings => html! {
+                    <SettingsPage {settings} {set_settings} />
+                },
+            }
+        }
+    });
 
     html! {
         <BrowserRouter>
-            <Switch<Route> render={move |r| switch_route(r, &settings)}/>
+            <Switch<Route> render={switch}/>
         </BrowserRouter>
     }
 }
